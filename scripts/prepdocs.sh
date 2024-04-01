27bd46b00358c68b26bf20a8dd2a4d4f2f7b6f85
davidwagaps <d.waghalter@gmail.com>
2024-04-01 23:31:00 +0000

    removing sample docs

--- scripts/prepdocs.sh
+++ scripts/prepdocs.sh
@@ -69,6 +69,7 @@ if [ -n "$USE_FEATURE_INT_VECTORIZATION" ]; then
 fi
 
 ./scripts/.venv/bin/python ./scripts/prepdocs.py './data/*' --verbose \
+--removeall \
 --subscriptionid $AZURE_SUBSCRIPTION_ID  \
 --storageaccount "$AZURE_STORAGE_ACCOUNT" --container "$AZURE_STORAGE_CONTAINER" --storageresourcegroup $AZURE_STORAGE_RESOURCE_GROUP \
 --searchservice "$AZURE_SEARCH_SERVICE" --index "$AZURE_SEARCH_INDEX" \
